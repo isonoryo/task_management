09ab369304cedbea31ca66ab972d9be3bcebc0c6
isonoryo <ljiye2422@gmail.com>
2020-01-15 16:24:07 +0900

    Add bootstrapの導入

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,13 +2,13 @@ class Task < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
   validates :limit, presence: true
-  # enum status: { "未着手": 1, "着手中": 2, "完了": 3 }
+
   enum priority: { "低": 1, "中": 2, "高": 3 }
-  # タイトル
+
   scope :search_title, ->(title) {
     where("title like ?", "%#{title}%")
     }
-  # ステータス
+
   scope :search_status, ->(status) {
     where(status: status)
     }
