0e8071bc18194ff2cb31c02f3174f9ee2a70f318
isonoryo <ljiye2422@gmail.com>
2020-01-15 20:23:04 +0900

    モデルテストの実装

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -21,15 +21,15 @@ RSpec.describe 'タスク管理機能', type: :model do
     @task = FactoryBot.create(:task)
   end
 
-  it 'search_title使用時、titleが一致した場合' do
+  it 'search_title実行時にtitleが一致した場合' do
     expect(Task.search_title("タイトル1")).to include(@task)
   end
 
-  it 'search_title使用時、titleのデータが存在しない場合' do
+  it 'search_title実行時にtitleのデータが存在しない場合' do
     expect(Task.search_title("失敗テスト")).not_to include(@task)
   end
 
-  it 'search_status使用時、statusに未着手を選択した場合' do
+  it 'search_status実行時にstatusに未着手を選択した場合' do
     expect(Task.search_status(1)).to include(@task)
   end
 
